4daafc340faef8d291c9e50736d94260170452e8
anhntn1 <anhntn1@hblab.vn>
2021-06-01 08:10:27 +0700

    remove small mesh

--- sph_data/main.py
+++ sph_data/main.py
@@ -1,4 +1,7 @@
 import sys
+
+import numpy as np
+
 sys.path.append("../libraries/libgeom")
 
 import pysplishsplash as sph
@@ -10,6 +13,7 @@ from airbag_sampler import create_airbag_pointclouds
 from lib import surface_reconstruct_marching_cube, remove_inside_mesh, surface_reconstruct_marching_cube_with_vis, loop_subdivision
 import glob
 import meshio
+from collections import Counter
 
 
 def build_shape(radius=(40, 30, 35), model_file='airbag.obj'):
@@ -39,6 +43,33 @@ def sph_simulation():
     base.run()
 
 
+def remove_noise_of_mesh(faces):
+    face_status = {(x, y, z): -1 for x, y, z in faces}
+
+    cnt = -1
+    while len(faces) > 0:
+        check_vertices = list(faces[0])
+        status = True
+
+        cnt += 1
+        flag = len(faces)
+        while status:
+            for face in faces:
+                if face[0] in check_vertices or face[1] in check_vertices or face[2] in check_vertices:
+                    check_vertices.extend(face)
+                    face_status[(face[0], face[1], face[2])] = cnt
+            faces = [k for k, v in face_status.items() if v == -1]
+            if len(faces) < flag:
+                flag = len(faces)
+            else:
+                status = False
+
+    values = Counter(list(face_status.values()))
+    max_key = max(values, key=values.get)
+    big_face = [k for k, v in face_status.items() if v == max_key]
+    return np.array(big_face)
+
+
 def vtk_to_mesh(vtk_folder='../data_heavy/sph_solutions/vtk/', if_vis=False):
     vtk_files = glob.glob(vtk_folder + '*.vtk')
     vtk_files = sorted(vtk_files, key=lambda x: int(x.split('_')[-1].split('.')[0]))
@@ -50,8 +81,13 @@ def vtk_to_mesh(vtk_folder='../data_heavy/sph_solutions/vtk/', if_vis=False):
 
         v, f = surface_reconstruct_marching_cube(pcd, cube_size=0.15, isovalue=0.14, verbose=False)
         v, f = loop_subdivision(v, f)
+
+        f = remove_noise_of_mesh(f)
+
         mesh = o3d.geometry.TriangleMesh(vertices=o3d.utility.Vector3dVector(v),
                                          triangles=o3d.utility.Vector3iVector(f))
+        o3d.visualization.draw_geometries([mesh])
+
         o3d.io.write_triangle_mesh("mc_solutions/%s.obj" % file.split("/")[-1].split(".")[0], mesh)
 
         mesh = o3d.geometry.TriangleMesh(vertices=o3d.utility.Vector3dVector(v),
@@ -85,6 +121,29 @@ def vtk_to_mesh(vtk_folder='../data_heavy/sph_solutions/vtk/', if_vis=False):
             vis.capture_screen_image("saved/im%d.png" % ind)
             ind += 1
     return list_mesh
+
+
+def draw_line(vertices, faces):
+    vis = o3d.visualization.Visualizer()
+    vis.create_window()
+    ctr = vis.get_view_control()
+
+    for i, face in enumerate(faces):
+        print(i)
+        lines = [[0, 1], [1, 2], [2, 0]]
+        colors = [[0, 0, 1] for i in range(len(lines))]
+        triangle_points = np.array([vertices[face[0]], vertices[face[1]], vertices[face[2]]])
+        line_pcd = o3d.geometry.LineSet()
+        line_pcd.lines = o3d.utility.Vector2iVector(lines)
+        line_pcd.colors = o3d.utility.Vector3dVector(colors)
+        line_pcd.points = o3d.utility.Vector3dVector(triangle_points)
+
+        vis.add_geometry(line_pcd)
+        ctr.rotate(10, 0.0)
+        vis.poll_events()
+        vis.update_renderer()
+
+    return
 
 
 if __name__ == "__main__":
