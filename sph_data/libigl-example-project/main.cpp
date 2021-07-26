e9886fb6df9a1f7235f8235baddcd6f76e4ac2cd
sontung <tung.nguyenson1404@gmail.com>
2021-07-26 08:37:47 +0700

    progress bar for cpp

--- sph_data/libigl-example-project/main.cpp
+++ sph_data/libigl-example-project/main.cpp
@@ -61,17 +61,31 @@ int main(int argc, char *argv[])
   string save_smooth_mesh = "../../mc_solutions_smoothed/";
 
   all_files = list_all_files(obj_folder);
+  float progress = 0.0;
+  int barWidth = 70;
+  int nb_files = all_files.size();
 
   for (auto file: all_files)
   {
       string in_path = obj_folder + file;
       string out_path = save_smooth_mesh + file;
+      cout << "processing mesh with libigl [";
+      int pos = barWidth * progress;
+      for (int i = 0; i < barWidth; ++i) {
+        if (i < pos) cout << "=";
+        else if (i == pos) cout << ">";
+        else cout << " ";
+      }
+      cout << "] " << int(progress * 100.0) << " %\r";
+      cout.flush();
 
       igl::readOBJ(in_path, V1, F1);
       surface_fairing(V1, F1, V2, F2);
       surface_smoothing(V2, F2, 2, V3, F3);
       up_sample(V3, F3, 2, V4, F4);
       igl::writeOBJ(out_path,V4,F4);
+
+      progress += 1.0/(float)nb_files;
 
 //      viewer.data().set_mesh(V3, F3);
 //      viewer.callback_key_down = key_down;
